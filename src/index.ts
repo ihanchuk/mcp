d7f881490e033052bd02631bbea3cab6f072020f
ihanchuk <mfc2005@ukr.net>
2026-06-05 23:01:36 +0300

    some initial NATS setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
- 
+import { connect } from "nats";
+import * as N from "nats";
+
+const nc = await connect({
+  name: "node-connection-main",
+  servers: "nats://localhost:4222",
+});
+
+const sc = N.StringCodec();
+
+// const sub = nc.subscribe("test.one");
+
+// (async () => {
+//   for await (const m of sub) {
+//     const counter = `[${sub.getProcessed()}]`;
+//     const mes = JSON.parse(m.string());
+
+//     console.log(`${counter} : from ${mes["from"]}`);
+//   }
+//   console.log("subscription closed");
+// })();
+
+const testOneSub = nc.subscribe("test.one", {
+  callback(err, msg) {
+    if (!err) {
+      const mes = JSON.parse(msg.string());
+      console.log(mes);
+      const mes2 = JSON.parse(sc.decode(msg.data));
+      console.log(mes2.from);
+    }
+  },
+  max: 1,
+});
+
+console.log("connected");
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,5 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio";
 
 import { registerMCPResources } from "./utils/registerResources";
 import { MCP_RESOURCES } from "./resources";
